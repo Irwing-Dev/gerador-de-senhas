95fe54217d2b10d037ce0f5e46a9f796648a8730
Irwing <irwing.devbr@gmail.com>
2023-03-12 15:20:23 -0300

    add pin and update index.html

---
+++ script.js
@@ -0,0 +1,46 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+let copied = document.getElementById("copied")
+
+let sizePassword = document.querySelector("#numero");
+let password = document.getElementById('password');
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/*#%&;!.,";
+let nums = "1234567890";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value
+}
+
+function generatePassword() {
+    
+    let pass = "";
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
+}
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha += pass; 
+}
+
+function generatePasswordPin() {
+
+    let pass = "";
+    for (let i = 0, n = nums.length; i < sliderElement.value; ++i) {
+        pass += nums.charAt(Math.floor(Math.random() * n));
+}
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha += pass; 
+}
+
+function copyPassword() {
+    navigator.clipboard.writeText(novaSenha)
+    novaSenha = " "
+    copied.style.display = "flex"
+}
